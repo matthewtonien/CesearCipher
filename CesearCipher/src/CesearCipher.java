8f8e118ae1a452a8c1f1e636b69fbeac678ad32c
matth <matth@LAPTOP-NIMKCTQC>
2021-07-12 09:59:33 +1000

    initial commit

---
+++ CesearCipher/src/CesearCipher.java
@@ -0,0 +1,133 @@
+
+/**
+ * 
+ * @author matthew tonien
+ * Cesear cipher program
+ */
+public class CesearCipher {
+	
+	/**
+	 * To encrypt a message
+	 * @param plaintext the message to be encrypted
+	 * @param key encryption key 
+	 * @return the function returns the ciphertext
+	 */
+	String encrypt(String plaintext, int key) {
+		
+		//TODO
+		return null;
+		
+	}
+	
+	/**
+	 * 
+	 * @param ciphertext the encrypted message
+	 * @param key encryption key
+	 * @return the function returns the plaintext
+	 */
+	String decrypt(String ciphertext, int key) {
+		
+		//TODO
+		return null;
+		
+	}
+	
+	/**
+	 * 
+	 * @param pLetter single plaintext letter
+	 * @param key encryption key
+	 * @return the function returns the cipher letter
+	 */
+	String encryptLetter(String pLetter, int key) {
+		
+		//TODO
+		return null;
+		
+	}
+	
+	/**
+	 * 
+	 * @param cLetter single ciphertext letter
+	 * @param key encryption key
+	 * @return the function returns the plaintext letter
+	 */
+	String decryptLetter(String cLetter, int key) {
+		
+		//TODO
+		return null;
+		
+	}
+	
+	/**
+	 * 
+	 * @param pLetter plaintext letter
+	 * @return the function returns true or false statement
+	 */
+	boolean isLetter(String pLetter) {
+		
+		//TODO
+		return null;
+		
+	}
+	
+	/**
+	 * 
+	 * @param pLetter plaintext letter
+	 * @return the function returns a true or false statement
+	 */
+	boolean isCapitalLetter(String pLetter) {
+		
+		//TODO
+		return null;
+		
+	}
+	
+	/**
+	 * 
+	 * @param pLetter plaintext letter
+	 * @return the function returns the index number
+	 */
+	int findIndexUpper(String pLetter) {
+		
+		//TODO
+		return null;
+		
+	}
+	
+	/**
+	 * 
+	 * @param pLetter plaintext letter
+	 * @return the function returns the index number
+	 */
+	int findIndexLower(String pLetter) {
+		
+		//TODO
+		return null;
+		
+	}
+	
+	/**
+	 * 
+	 * @param index index number
+	 * @return the function returns a cipher letter
+	 */
+	String findLetterUpper(int index) {
+		
+		//TODO
+		return null;
+		
+	}
+	
+	/**
+	 * 
+	 * @param index index number
+	 * @return the function returns a cipher letter
+	 */
+	String findLetterLower(int index) {
+		
+		//TODO
+		return null;
+		
+	}
+
+}
